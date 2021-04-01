253d4551494210d9da9f43d8b908affee50ac6ec
Samyak Shah <69042190+Sam777726@users.noreply.github.com>
2021-04-01 12:00:21 +0530

    changed for mysql integration

--- MiniProject_Backend.py
+++ MiniProject_Backend.py
@@ -1,47 +1,60 @@
 #backend
-import sqlite3
-
-def MovieData():
-    con=sqlite3.connect("movie1.db") 
-    cur=con.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, Movie_ID text,Movie_Name text,Release_Date text,Director text,Cast text,Budget text,Duration text,Rating text")
-    con.commit()
-    con.close()
+import mysql.connector
     
-def AddMovieRec(Movie_ID,Movie_Name,Release_Date,Director,Cast,Budget,Duration,Rating):
-    con=sqlite3.connect("movie1.db")    
-    cur=con.cursor()
-    cur.execute("INSERT INTO book VALUES (NULL, ?,?,?,?,?,?,?,?)", (Movie_ID,Movie_Name,Release_Date,Director,Cast,Budget,Duration,Rating))
-    con.commit()
-    con.close()
+def AddMovieRec(M_ID,Movie_Name,Release_Date,Director,Cast,Budget,Duration,Rating):
+    connection = mysql.connector.connect(host='localhost',
+                                            database='project',
+                                            user='root',
+                                            password='admin',
+                                            charset='utf8')
+    cur = connection.cursor()
+    cur.execute("INSERT INTO movie VALUES (NULL, ?,?,?,?,?,?,?,?)", (M_ID,Movie_Name,Release_Date,Director,Cast,Budget,Duration,Rating))
+    connection.commit()
+    connection.close()
 
 def ViewMovieData():
-    con=sqlite3.connect("movie1.db")    
-    cur=con.cursor()
-    cur.execute("SELECT * FROM book")
+    connection = mysql.connector.connect(host='localhost',
+                                            database='project',
+                                            user='root',
+                                            password='admin',
+                                            charset='utf8')
+    cur = connection.cursor()
+    cur.execute("SELECT * FROM movie")
     rows=cur.fetchall()
-    con.close()    
+    connection.close()    
     return rows
 
 def DeleteMovieRec(id):    
-    con=sqlite3.connect("movie1.db")    
-    cur=con.cursor()
-    cur.execute("DELETE FROM book WHERE id=?", (id,))
-    con.commit()
-    con.close()  
+    connection = mysql.connector.connect(host='localhost',
+                                            database='project',
+                                            user='root',
+                                            password='admin',
+                                            charset='utf8')
+    cur = connection.cursor()
+    cur.execute("DELETE FROM movie WHERE id=?", (id,))
+    connection.commit()
+    connection.close()  
 
-def SearchMovieData(Movie_ID="",Movie_Name="",Release_Date="",Director="",Cast="",Budget="",Duration="",Rating=""):  
-    con=sqlite3.connect("movie1.db")    
-    cur=con.cursor()
-    cur.execute("SELECT * FROM book WHERE Movie_ID=? OR Movie_Name=? OR Release_Date=? OR Director=? OR Cast=? OR Budget=? OR Duration=? OR Rating=?",(Movie_ID,Movie_Name,Release_Date,Director,Cast,Budget,Duration,Rating))
+def SearchMovieData(M_ID="",Movie_Name="",Release_Date="",Director="",Cast="",Budget="",Duration="",Rating=""):  
+    connection = mysql.connector.connect(host='localhost',
+                                            database='project',
+                                            user='root',
+                                            password='admin',
+                                            charset='utf8')
+    cur = connection.cursor()
+    cur.execute("SELECT * FROM movie WHERE M_ID=? OR Movie_Name=? OR Release_Date=? OR Director=? OR Cast=? OR Budget=? OR Duration=? OR Rating=?",(M_ID,Movie_Name,Release_Date,Director,Cast,Budget,Duration,Rating))
     rows=cur.fetchall()
-    con.close()    
+    connection.close()    
     return rows
 
-def UpdateMovieData(id,Movie_ID="",Movie_Name="",Release_Date="",Director="",Cast="",Budget="",Duration="",Rating=""):
-    con=sqlite3.connect("movie1.db")    
-    cur=con.cursor()
-    cur.execute("UPDATE book SET Movie_ID=?,Movie_Name=?,Release_Date=?,Director=?,Cast=?,Budget=?,Duration=?,Rating=?, WHERE id=?",(Movie_ID,Movie_Name,Release_Date,Director,Cast,Budget,Duration,Rating))
-    con.commit()
-    con.close()
+def UpdateMovieData(id,M_ID="",Movie_Name="",Release_Date="",Director="",Cast="",Budget="",Duration="",Rating=""):
+    connection = mysql.connector.connect(host='localhost',
+                                            database='project',
+                                            user='root',
+                                            password='admin',
+                                            charset='utf8')
+    cur = connection.cursor()
+    cur.execute("UPDATE movie SET M_ID=?,Movie_Name=?,Release_Date=?,Director=?,Cast=?,Budget=?,Duration=?,Rating=?, WHERE id=?",(M_ID,Movie_Name,Release_Date,Director,Cast,Budget,Duration,Rating))
+    connection.commit()
+    connection.close()
 
